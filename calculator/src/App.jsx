93f9ba501a1b4370e0a53b1d2bf24ea1db936ad3
Your Name <jomerubaldo@gmail.com>
2025-08-29 23:27:54 +0800

    initial commit

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -3,62 +3,64 @@ function App() {
     // background color theme
     <div className="flex justify-center items-center bg-black w-screen h-screen">
       {/* background color calculator */}
-      <div className=" bg-[#21ced1] w-96 h-96 rounded-lg pt-5">
-        <div className="bg-white w-[350px] h-16 rounded-md">Display</div>
-        <div className="flex justify-center gap-2">
-          <div>
-            <button className="bg-black w-[75px] h-14 rounded-md text-white text-center font-bold">
+      <div className=" bg-[#21ced1] w-96 h-[360px] rounded-lg pt-5 flex flex-col items-center">
+        <div className="bg-white w-[345px] h-16 rounded-md text-right pt-2 pr-1 font-bold text-5xl">
+          0
+        </div>
+        <div className="grid gap-2  pt-2  ">
+          <div className="space-x-2">
+            <button className="bg-black w-[80px] h-14 rounded-md text-white text-center font-bold">
               1
             </button>
-            <button className="bg-black w-[75px] h-14 rounded-md text-white text-center font-bold">
-              4
-            </button>
-            <button className="bg-black w-[75px] h-14 rounded-md text-white text-center font-bold">
-              7
-            </button>
-            <button className="bg-black w-[75px] h-14 rounded-md text-white text-center font-bold">
-              C
-            </button>
-          </div>
-          <div>
-            <button className="bg-black w-[75px] h-14 rounded-md text-white text-center font-bold">
+            <button className="bg-black w-[80px] h-14 rounded-md text-white text-center font-bold">
               2
             </button>
-            <button className="bg-black w-[75px] h-14 rounded-md text-white text-center font-bold">
-              5
-            </button>
-            <button className="bg-black w-[75px] h-14 rounded-md text-white text-center font-bold">
-              8
-            </button>
-            <button className="bg-black w-[75px] h-14 rounded-md text-white text-center font-bold">
-              0
-            </button>
-          </div>
-          <div>
-            <button className="bg-black w-[75px] h-14 rounded-md text-white text-center font-bold">
+            <button className="bg-black w-[80px] h-14 rounded-md text-white text-center font-bold">
               3
             </button>
-            <button className="bg-black w-[75px] h-14 rounded-md text-white text-center font-bold">
-              6
-            </button>
-            <button className="bg-black w-[75px] h-14 rounded-md text-white text-center font-bold">
-              9
-            </button>
-            <button className="bg-black w-[75px] h-14 rounded-md text-white text-center font-bold">
-              =
-            </button>
-          </div>
-          <div>
-            <button className="bg-black w-[75px] h-14 rounded-md text-white text-center font-bold">
+            <button className="bg-black w-[80px] h-14 rounded-md text-white text-center font-bold">
               +
             </button>
-            <button className="bg-black w-[75px] h-14 rounded-md text-white text-center font-bold">
+          </div>
+          <div className="space-x-2">
+            <button className="bg-black w-[80px] h-14 rounded-md text-white text-center font-bold">
+              4
+            </button>
+            <button className="bg-black w-[80px] h-14 rounded-md text-white text-center font-bold">
+              5
+            </button>
+            <button className="bg-black w-[80px] h-14 rounded-md text-white text-center font-bold">
+              6
+            </button>
+            <button className="bg-black w-[80px] h-14 rounded-md text-white text-center font-bold">
               -
             </button>
-            <button className="bg-black w-[75px] h-14 rounded-md text-white text-center font-bold">
+          </div>
+          <div className="space-x-2">
+            <button className="bg-black w-[80px] h-14 rounded-md text-white text-center font-bold">
+              7
+            </button>
+            <button className="bg-black w-[80px] h-14 rounded-md text-white text-center font-bold">
+              8
+            </button>
+            <button className="bg-black w-[80px] h-14 rounded-md text-white text-center font-bold">
+              9
+            </button>
+            <button className="bg-black w-[80px] h-14 rounded-md text-white text-center font-bold">
               *
             </button>
-            <button className="bg-black w-[75px] h-14 rounded-md text-white text-center font-bold">
+          </div>
+          <div className="space-x-2">
+            <button className="bg-black w-[80px] h-14 rounded-md text-white text-center font-bold">
+              C
+            </button>
+            <button className="bg-black w-[80px] h-14 rounded-md text-white text-center font-bold">
+              0
+            </button>
+            <button className="bg-black w-[80px] h-14 rounded-md text-white text-center font-bold">
+              =
+            </button>
+            <button className="bg-black w-[80px] h-14 rounded-md text-white text-center font-bold">
               /
             </button>
           </div>
